232689c0f78b843bcbddaee4e848db72b15a2379
GabrielViniciusss <biel23061@gmail.com>
2023-12-19 23:42:43 -0300

    prova 2022.2

--- Haskell/p1.hs
+++ Haskell/p1.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 rlencode0 :: [Int] -> [Int]
 rlencode0 [] = []
 rlencode0 (x:xs)
@@ -16,4 +18,19 @@ rldecode0 (x:xs:t)
     | x /= 0 = x : rldecode0 (xs:t)
     | x == 0 = x : (replicate (xs-1) x) ++ rldecode0 t
 rldecode0(x:[]) = [x]
+
+
+rlencodeLetras :: String -> String
+rlencodeLetras [] = []
+rlencodeLetras [x] = [x]
+rlencodeLetras (x:xs)
+    |x == head xs = x : show (length (takeWhile (==x) xs) + 1) ++ rlencodeLetras (dropWhile (==x) xs)
+    |otherwise = x : rlencodeLetras xs
+
+rldecodeLetras :: String -> String
+rldecodeLetras [] = []
+rldecodeLetras [x] = [x]
+rldecodeLetras (x:xs:t)
+    |isDigit xs = x : replicate (digitToInt xs - 1) x ++ rldecodeLetras t
+    |otherwise = x: rldecodeLetras (xs:t)
 
